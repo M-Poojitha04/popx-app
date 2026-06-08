62a3931951caf62feb7e744504c7683d2847b04c
Poojitha Machrla <poojitha.machrla@gmail.com>
2026-06-08 19:59:00 +0530

    Refactor CSS structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./styles.css";
+import "./styles/global.css";
+import "./styles/welcome.css";
+import "./styles/auth.css";
+import "./styles/profile.css";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
